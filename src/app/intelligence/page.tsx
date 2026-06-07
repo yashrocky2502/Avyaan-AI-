b76f2ca49b45a73919aca92f6426119bdb340e03
yashrocky2502 <yashrocky2502@gmail.com>
2026-06-07 14:50:02 +0530

    Add files via upload

---
+++ src/app/intelligence/page.tsx
@@ -0,0 +1,12 @@
+
+'use client';
+
+import { MarketIntelligenceHub } from "@/components/intelligence/market-intelligence-hub";
+
+export default function IntelligencePage() {
+  return (
+    <div className="animate-in fade-in slide-in-from-bottom-4 duration-1000">
+      <MarketIntelligenceHub />
+    </div>
+  );
+}
