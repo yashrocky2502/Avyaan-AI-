189602175706e62a928366bca5e8cfe84d7b87c0
yashrocky2502 <yashrocky2502@gmail.com>
2026-06-07 14:49:05 +0530

    Add files via upload

---
+++ src/app/changes/page.tsx
@@ -0,0 +1,12 @@
+
+'use client';
+
+import { MarketChanges } from "@/components/dashboard/market-changes";
+
+export default function ChangesPage() {
+  return (
+    <div className="animate-in fade-in slide-in-from-bottom-4 duration-700">
+      <MarketChanges />
+    </div>
+  );
+}
