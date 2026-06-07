49a69b8c6fdc6a1bd4cd4e2e4c435f8d23d987ac
yashrocky2502 <yashrocky2502@gmail.com>
2026-06-07 14:50:48 +0530

    Add files via upload

---
+++ src/app/ipo/page.tsx
@@ -0,0 +1,12 @@
+
+'use client';
+
+import { IpoExplorer } from "@/components/ipo/ipo-explorer";
+
+export default function IpoPage() {
+  return (
+    <div className="animate-in fade-in slide-in-from-bottom-4 duration-1000">
+      <IpoExplorer />
+    </div>
+  );
+}
