06089c3cb01d67d4803572e5327567d07b252cf8
yashrocky2502 <yashrocky2502@gmail.com>
2026-06-07 14:55:21 +0530

    Add files via upload

---
+++ src/app/research/page.tsx
@@ -0,0 +1,12 @@
+
+'use client';
+
+import { ResearchHub } from "@/components/research/research-hub";
+
+export default function ResearchPage() {
+  return (
+    <div className="animate-in fade-in slide-in-from-bottom-4 duration-700">
+      <ResearchHub />
+    </div>
+  );
+}
