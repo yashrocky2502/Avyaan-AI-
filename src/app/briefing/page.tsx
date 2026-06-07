cca3c859c43c82524b3915e56af954718ae4a8ff
yashrocky2502 <yashrocky2502@gmail.com>
2026-06-07 14:48:13 +0530

    Add files via upload

---
+++ src/app/briefing/page.tsx
@@ -0,0 +1,12 @@
+
+'use client';
+
+import { DailyBriefing } from "@/components/dashboard/daily-briefing";
+
+export default function BriefingPage() {
+  return (
+    <div className="animate-in fade-in slide-in-from-bottom-4 duration-1000">
+      <DailyBriefing />
+    </div>
+  );
+}
